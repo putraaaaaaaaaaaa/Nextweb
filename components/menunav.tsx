00a4f39fc5a36eae3c2df89785a2a091e694aa8e
putraaaaaaaaaaaa <sedo31149@gmail.com>
2024-06-06 15:57:52 +0700

    update menunav.tsx

--- components/menunav.tsx
+++ components/menunav.tsx
@@ -25,21 +25,21 @@ export default function MyModal() {
         className="rounded-md bg-secondary p-2 text-foreground lg:hidden"
       >
         <span class="sr-only">Open menu</span>
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  strokeWidth="1.5"
-  stroke="currentColor"
-  aria-hidden="true"
-  className="h-6 w-6"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-  />
-</svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          aria-hidden="true"
+          className="h-6 w-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
       </Button>
       <Transition appear show={isOpen}>
         <Dialog
