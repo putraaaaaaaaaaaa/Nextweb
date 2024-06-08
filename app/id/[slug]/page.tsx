94be92a239e5dcf3de9fd4e9cfd7fae43130300c
putraaaaaaaaaaaa <sedo31149@gmail.com>
2024-06-08 14:02:58 +0700

    update

--- app/id/[slug]/page.tsx
+++ app/id/[slug]/page.tsx
@@ -1,3 +1,5 @@
+// app/id/[slug]/page.tsx
+
 "use client";
 import { notFound } from "next/navigation";
 import {
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import "./app.css";
+import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navbar";
