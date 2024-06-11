f1fd9676dbd918c031049f80555d0a6c567bb6fc
putraaaaaaaaaaaa <sedo31149@gmail.com>
2024-06-11 21:39:26 +0700

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Dm_Sans as FontSans } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import "./app.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
