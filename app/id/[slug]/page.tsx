fadb97d19927384ebda659d9ba79c0fd87e7bab6
putraaaaaaaaaaaa <75620395+putraaaaaaaaaaaa@users.noreply.github.com>
2024-06-15 03:15:43 +0000

    V

--- app/id/[slug]/page.tsx
+++ app/id/[slug]/page.tsx
@@ -1,10 +1,12 @@
 "use client";
-import React, { useState, ReactNode, useContext, MouseEvent } from "react";
+import React, { useState, useContext, MouseEvent } from "react";
 import { notFound } from "next/navigation";
 import {
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
+  Description,
+  Input,
   Transition,
   Field,
   Label,
@@ -18,11 +20,8 @@ import Tag from "@/components/order/tag";
 import Deskripsi from "@/components/order/deskripsi";
 import { formatIDR } from "@/lib/formatIDR";
 import { toast } from "react-hot-toast";
-
-// Move data arrays to a separate file
 import { productCategories } from "@/data/denom";
 
-// Create a context for managing the selected product state
 interface ProductContextType {
   selected: null | {
     name: string;
@@ -30,39 +29,19 @@ interface ProductContextType {
     harga: number;
     image?: string;
   };
-  setSelected: (selected: null | {
-    name: string;
-    kode: string;
-    harga: number;
-    image?: string;
-  }) => void;
+  setSelected: (
+    selected: null | {
+      name: string;
+      kode: string;
+      harga: number;
+      image?: string;
+    },
+  ) => void;
 }
 
 const ProductContext = React.createContext<ProductContextType | null>(null);
 
-interface PageProps {
-  params: {
-    slug: string | null;
-  };
-}
-
-const Page: React.FC<PageProps> = ({ params }) => {
-  const { slug } = params;
-  const isValidSlug = ["mobile-legends", "b"].includes(slug || "");
-
-  if (!isValidSlug) {
-    notFound();
-    return null; // Early return if slug is invalid
-  }
-
-  return (
-    <ProductProvider>
-      {slug!== null && <Main slug={slug} />}
-    </ProductProvider>
-  );
-};
-
-const ProductProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+const Page: React.FC = () => {
   const [selected, setSelected] = useState<null | {
     name: string;
     kode: string;
@@ -70,29 +49,61 @@ const ProductProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     image?: string;
   }>(null);
 
-  return (
-    <ProductContext.Provider value={{ selected, setSelected }}>
-      {children}
-    </ProductContext.Provider>
-  );
-};
+  const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
 
-const Main: React.FC<{ slug: string }> = ({ slug }) => {
+    if (!selected) {
+      toast.error("Silahkan pilih nominal terlebih dahulu");
+      return;
+    }
+
+    const id = document.querySelector<HTMLInputElement>('[name="id"]');
+    const server = document.querySelector<HTMLInputElement>('[name="server"]');
+
+    if (!id || !server) {
+      toast.error("Silahkan isi ID dan Server terlebih dahulu");
+      return;
+    }
+
+    if (id.value.trim() === "" || server.value.trim() === "") {
+      toast.error("ID dan Server tidak boleh kosong");
+      return;
+    }
+
+    const apiUrl = "https://vip-reseller.co.id/api/game-feature"; // Replace with your API URL
+    const data = {
+      key: "P7S6yQhfMkl8y4AjprKIMMJ85R61i8SpP5qQCmJa8oYx4UcLo5leoHL4cQQgZo54",
+      sign: "a895bd30ee97625749f091612a0d25e2",
+      type: "get-nickname",
+      code: "mobile-legends",
+      target: id.value,
+      additional_target: server.value,
+    };
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams(data),
+    };
+
+    fetch(apiUrl, config).then(async (response) => {
+      const data = await response.json();
+      const result = data.result;
+      if (result === true) {
+        toast.success(`Username ${data.data}`);
+      } else if (result === false) {
+        toast.error(`${data.message}`);
+      }
+    });
+  };
+
+  const filteredCategories = productCategories.filter(
+    (category) => category.slug === "mobile-legends",
+  );
+
   return (
     <main className="relative bg-background">
-      <Header slug={slug} />
-      <div className="container relative mt-8 grid grid-cols-3 gap-4 md:gap-8">
-        <Deskripsi />
-        <ProductOptions slug={slug} />
-      </div>
-      <Foot />
-    </main>
-  );
-};
-
-const Header: React.FC<{ slug: string }> = ({ slug }) => {
-  return (
-    <>
       <div className="relative h-56 w-full bg-muted lg:h-[340px]"></div>
       <div className="bg-title-product flex min-h-32 w-full items-center border-y bg-muted lg:min-h-[160px] bg-order-header-background text-order-header-foreground">
         <div className="container flex items-center gap-2">
@@ -118,158 +129,136 @@ const Header: React.FC<{ slug: string }> = ({ slug }) => {
           </div>
         </div>
       </div>
-    </>
-  );
-};
-
-interface ProductCategoryProps {
-  category: {
-    emoji: string;
-    name: string;
-    slug: string;
-    products: {
-      kode: string;
-      name: string;
-      harga: number;
-      image?: string;
-    }[];
-  };
-  selected: null | {
-    name: string;
-    kode: string;
-    harga: number;
-    image?: string;
-  };
-  setSelected: (selected: null | {
-    name: string;
-    kode: string;
-    harga: number;
-    image?: string;
-  }) => void;
-}
-
-const ProductCategory: React.FC<ProductCategoryProps> = ({ category, selected, setSelected }) => {
-  return (
-    <section id={`${category.emoji} ${category.name}`}>
-      <h3 className="pb-4 text-sm/6 font-semibold text-card-foreground">
-        {category.emoji} {category.name}
-      </h3>
-      <RadioGroup
-        by="name"
-        value={selected}
-        onChange={setSelected}
-        aria-label={category.name}
-      >
-        <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3">
-          {category.products.map((denom) => (
-            <ProductOption key={denom.kode} denom={denom} />
-          ))}
-        </div>
-      </RadioGroup>
-    </section>
-  );
-};
-
-interface ProductOptionsProps {
-  slug: string;
-}
-
-  const ProductOptions: React.FC<ProductOptionsProps> = ({ slug }) => {
-    const { selected, setSelected } = useContext(ProductContext)?? {};
-
-  const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-
-    if (!selected) {
-      toast.error("Silahkan pilih nominal terlebih dahulu");
-      return;
-    }
-
-    toast.success(`Item selected: ${selected.name}`);
-    // Add any additional logic or actions you want to perform when a product is selected
-  };
-
-  const filteredCategories = productCategories.filter(
-    (category) => category.slug === slug,
-  );
-
-  return (
-    <form className="col-span-3 col-start-1 flex flex-col gap-4 lg:col-span-2 lg:gap-8">
-      <section className="relative rounded-xl bg-card/50 shadow-2xl" id="1">
-        <div className="flex items-center overflow-hidden rounded-t-xl bg-card">
-          <div className="flex h-10 w-10 items-center justify-center bg-primary font-semibold text-primary-foreground">
-            1
-          </div>
-          <h2 className="px-4 py-2 text-sm/6 font-semibold text-card-foreground">
-            Pilih Nominal
-          </h2>
-        </div>
-        <div className="p-4">
-          <div className="flex flex-col space-y-4">
-            {filteredCategories.map((category) => (
-              <ProductCategory
-                key={category.name}
-                category={category}
-                selected={selected?? null} // Add the nullish coalescing operator (??) to default to null if selected is undefined
-                setSelected={setSelected?? (() => {})}
-              />
-            ))}
-          </div>
-        </div>
-      </section>
-      <button onClick={handleButtonClick}>Submit</button>
-    </form>
-  );
-};
-
-interface ProductOptionProps {
-  denom: {
-    kode: string;
-    name: string;
-    harga: number;
-    image?: string;
-  };
-}
-
-  const ProductOption: React.FC<ProductOptionProps> = ({ denom }) => {
-    const { selected, setSelected } = useContext(ProductContext)?? {};
-
-  return (
-    <Radio
-      as="div"
-      value={denom}
-      className={`relative flex cursor-pointer rounded-xl border border-transparent bg-foreground/75 p-2.5 text-background shadow-sm outline-none md:p-4 bg-order-variant-background  text-order-variant-foreground ${
-        denom === selected
-         ? "ring-2 neko-shadow ring-offset-card ring-offset-2 ring-primary"
-          : ""
-      }`}
-    >
-      <span className="flex flex-1">
-        <span className="flex flex-col justify-between">
-          <span className="block text-xs font-semibold">{denom.name}</span>
-          <div>
-            <span
-              className={`mt-1 flex items-center text-xs font-semibold ${
-                denom === selected? "text-primary" : ""
-              }`}
-            >
-              {formatIDR(denom.harga)}
-            </span>
-          </div>
-        </span>
-      </span>
-      {denom.image? (
-        <div className="flex aspect-square w-8 items-center">
-          <Image
-            alt={denom.name}
-            src={denom.image}
-            width={300}
-            height={300}
-            className="object-contain object-right"
-          />
-        </div>
-      ) : null}
-    </Radio>
+      <div className="container relative mt-8 grid grid-cols-3 gap-4 md:gap-8">
+        <Deskripsi />
+        <form className="col-span-3 col-start-1 flex flex-col gap-4 lg:col-span-2 lg:gap-8">
+          <section className="relative rounded-xl bg-card/50 shadow-2xl" id="1">
+            <div className="flex items-center overflow-hidden rounded-t-xl bg-card">
+              <div className="flex h-10 w-10 items-center justify-center bg-primary font-semibold text-primary-foreground">
+                1
+              </div>
+              <h2 className="px-4 py-2 text-sm/6 font-semibold text-card-foreground">
+                Pilih Nominal
+              </h2>
+            </div>
+            <div className="p-4">
+              <div className="flex flex-col space-y-4">
+                {filteredCategories.map((category) => (
+                  <div key={category.name}>
+                    <h3 className="pb-4 text-sm/6 font-semibold text-card-foreground">
+                      {category.emoji} {category.name}
+                    </h3>
+                    <RadioGroup
+                      by="name"
+                      value={selected}
+                      onChange={setSelected}
+                      aria-label={category.name}
+                    >
+                      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3">
+                        {category.products.map((denom) => (
+                          <Radio
+                            as="div"
+                            value={denom}
+                            key={denom.kode}
+                            className={`relative flex cursor-pointer rounded-xl border border-transparent bg-foreground/75 p-2.5 text-background shadow-sm outline-none md:p-4 bg-order-variant-background  text-order-variant-foreground ${
+                              denom === selected
+                                ? "ring-2 neko-shadow ring-offset-card ring-offset-2 ring-primary"
+                                : ""
+                            }`}
+                          >
+                            <span className="flex flex-1">
+                              <span className="flex flex-col justify-between">
+                                <span className="block text-xs font-semibold">
+                                  {denom.name}
+                                </span>
+                                <div>
+                                  <span
+                                    className={`mt-1 flex items-center text-xs font-semibold ${
+                                      denom === selected ? "text-primary" : ""
+                                    }`}
+                                  >
+                                    {formatIDR(denom.harga)}
+                                  </span>
+                                </div>
+                              </span>
+                            </span>
+                            {denom.image ? (
+                              <div className="flex aspect-square w-8 items-center">
+                                <Image
+                                  alt={denom.name}
+                                  src={denom.image}
+                                  width={300}
+                                  height={300}
+                                  className="object-contain object-right"
+                                />
+                              </div>
+                            ) : null}
+                          </Radio>
+                        ))}
+                      </div>
+                    </RadioGroup>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
+          <section className="relative rounded-xl bg-card/50 shadow-2xl" id="2">
+            <div className="flex items-center overflow-hidden rounded-t-xl bg-card">
+              <div className="flex h-10 w-10 items-center justify-center bg-primary font-semibold text-primary-foreground">
+                2
+              </div>
+              <h2 className="px-4 py-2 text-sm/6 font-semibold text-card-foreground">
+                Masukan Data Akun
+              </h2>
+            </div>
+            <div className="p-4">
+              <div className="grid grid-cols-2 gap-4">
+                <Field>
+                  <Label className="block text-xs font-medium text-foreground pb-2">
+                    ID
+                  </Label>
+                  <div className="flex flex-col items-start">
+                    <Input
+                      className="relative block w-full appearance-none rounded-lg border border-border bg-input px-3 py-2 text-xs text-foreground placeholder-muted-foreground focus:z-10 focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary disabled:cursor-not-allowed disabled:opacity-75"
+                      type="number"
+                      name="id"
+                      placeholder="Ketikan ID"
+                    />
+                  </div>
+                </Field>
+                <Field>
+                  <Label className="block text-xs font-medium text-foreground pb-2">
+                    Server
+                  </Label>
+                  <div className="flex flex-col items-start">
+                    <Input
+                      className="relative block w-full appearance-none rounded-lg border border-border bg-input px-3 py-2 text-xs text-foreground placeholder-muted-foreground focus:z-10 focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary disabled:cursor-not-allowed disabled:opacity-75"
+                      type="number"
+                      name="server"
+                      placeholder="Ketikan Server"
+                    />
+                  </div>
+                </Field>
+              </div>
+              <div className="mt-4 text-xs text-card-foreground">
+                <div>
+                  <p>
+                    <em>
+                      Untuk menemukan ID & Server akun Anda, klik avatar Anda di
+                      pojok kiri atas layar dan buka tab Info Umum. Contoh:
+                      12345678 (9864), maka ID adalah 12345678 dan Server adalah
+                      9864
+                    </em>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+          <button onClick={handleButtonClick}>Submit</button>
+        </form>
+      </div>
+      <Foot />
+    </main>
   );
 };
 
