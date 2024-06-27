f99d60528dbb65d016aefbdbdb9d522773083064
putraaaaaaaaaaaa <75620395+putraaaaaaaaaaaa@users.noreply.github.com>
2024-06-27 10:12:07 +0000

    Logic

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -131,6 +131,14 @@ export default function Login() {
                   </div>
                 </div>
                 <div>
+                  {error && (
+                    <div
+                      className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                      role="alert"
+                    >
+                      <span className="block sm:inline">{error}</span>
+                    </div>
+                  )}
                   <button
                     className="items-center justify-center rounded-lg bg-primary px-4 py-2 text-sm font-medium text-primary-foreground duration-300 disabled:cursor-not-allowed disabled:opacity-75 group relative flex w-full hover:bg-primary/75"
                     type="submit"
