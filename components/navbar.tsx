f0dfeae82f71de5e88bcc2a6819a9dbca9bab23b
putraaaaaaaaaaaa <75620395+putraaaaaaaaaaaa@users.noreply.github.com>
2024-06-30 03:59:10 +0000

    G

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import MyModal from "@/components/menunav";
 import Drop from "@/components/drop";
+import { User } from './user';
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import {
   ArchiveBoxXMarkIcon,
@@ -43,45 +44,7 @@ const Navbar = () => {
           <div className="ml-auto flex h-full items-center space-x-2 lg:space-x-6">
             <div className="flex flex-row-reverse items-center gap-x-2">
               <div className="relative inline-block text-left">
-                <Menu>
-                  <MenuButton className="inline-flex w-full items-center justify-center gap-x-2 rounded-lg border border-border/50 bg-transparent py-2 pl-4 pr-3 text-sm font-semibold uppercase text-foreground duration-300 ease-in-out hover:bg-muted/50">
-                    <CircleUser size={20} />
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                      className="h-4 w-4"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </MenuButton>
-                  <MenuItems
-                    as="div"
-                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right divide-y divide-muted rounded-md bg-background shadow-lg ring-1 ring-muted ring-opacity-5 focus:outline-none transition ease-in duration-75 transform opacity-100 scale-100"
-                  >
-                    <div className="py-1">
-                      <MenuItem>
-                        <button className="text-foreground group flex w-full items-center px-4 py-2 text-sm">
-                          <Link className="ml-4" href="/sign-in">
-                            Masuk
-                          </Link>
-                        </button>
-                      </MenuItem>
-                      <MenuItem>
-                        <button className="text-foreground group flex w-full items-center px-4 py-2 text-sm">
-                          <Link className="ml-4" href="/sign-up">
-                            Daftar
-                          </Link>
-                        </button>
-                      </MenuItem>
-                    </div>
-                  </MenuItems>
-                </Menu>
+                <User />
               </div>
             </div>
           </div>
--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,6 @@
-import { NextRequest } from "next/server";
-import { authMiddleware } from "next-firebase-auth-edge";
-import { clientConfig, serverConfig } from "./config";
+export { auth as middleware } from '@/lib/auth';
 
-export async function middleware(request: NextRequest) {
-  return authMiddleware(request, {
-    loginPath: "/api/sign-in",
-    logoutPath: "/api/sign-out",
-    apiKey: clientConfig.apiKey,
-    cookieName: serverConfig.cookieName,
-    cookieSignatureKeys: serverConfig.cookieSignatureKeys,
-    cookieSerializeOptions: serverConfig.cookieSerializeOptions,
-    serviceAccount: serverConfig.serviceAccount,
-  });
-}
-
+// Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: ["/", "/((?!_next|api|.*\\.).*)", "/api/sign-in", "/api/sign-out"],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
